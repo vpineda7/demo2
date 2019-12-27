2e2d72549fd2a1d70a1a7112b31390dc845337f8
vpineda7 <vladimir.pineda7@gmail.com>
2019-12-27 02:24:31 -0600

    Updates

---
+++ component---src-pages-index-js-640cdbd4de70753fcfdc.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"14Y0":function(t,e,n){t.exports=n.p+"static/otter-be0dbc8400b352fa801b4a617d3b1c8e.gif"},RXBc:function(t,e,n){"use strict";n.r(e);var a=n("q1tI"),i=n.n(a),c=n("14Y0"),o=n.n(c);e.default=function(){return i.a.createElement("div",null,"Hello world! modified  ",i.a.createElement("img",{src:o.a,alt:"Otter dancing with a fish"}))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-640cdbd4de70753fcfdc.js.map
--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-import React from "react"
-
-import otterGIF from '../gifs/otter.gif'
-export default () => <div>Hello world! modified  <img src={otterGIF} alt="Otter dancing with a fish" /></div>
