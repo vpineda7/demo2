c27407b24d45e0e6e0f3ff52a06c3a9d2a83db23
vpineda7 <vladimir.pineda7@gmail.com>
2019-12-27 02:42:59 -0600

    Updates

---
+++ component---src-pages-index-js-b6d520eead81297d2009.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"14Y0":function(e,t,n){e.exports=n.p+"static/otter-2ac93011db01822e89a9b98bfb94b119.gif"},RXBc:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),i=n.n(a),c=n("14Y0"),r=n.n(c);t.default=function(){return i.a.createElement("div",null,"Hello world! modified 2",i.a.createElement("br",null),i.a.createElement("img",{src:r.a,alt:"Otter dancing with a fish"})," ")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-b6d520eead81297d2009.js.map
